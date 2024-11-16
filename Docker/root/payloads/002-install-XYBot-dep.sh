3d5c28e6226e0b9de3d898b403e90d7b173d23e5
wuhao1477 <wuhao1477@qq.com>
2024-11-17 03:22:43 +0800

    fix: 修复bug

--- Docker/root/payloads/002-install-XYBot-dep.sh
+++ Docker/root/payloads/002-install-XYBot-dep.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
 # ...existing code...
-
-# 设置时区为上海/北京
-ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-echo 'Asia/Shanghai' >/etc/timezone
-
-# ...existing code...
 cd /home/app/XYBot || exit
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
