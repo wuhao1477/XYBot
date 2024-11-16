94720fcfbf7eb992f0ffb78c34d6c023bf4223e4
wuhao1477 <wuhao1477@qq.com>
2024-11-17 02:45:07 +0800

    fix: 修复时区错误

--- Docker/root/payloads/001-clone-XYBot.sh
+++ Docker/root/payloads/001-clone-XYBot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /home/app || exit
-git clone https://github.com/wuhao1477/xybot.git
+git clone https://github.com/wuhao1477/xybot.git XYBot
 cd XYBot || exit
 # 删除以下行或修改为实际存在的分支
 # git checkout dev
