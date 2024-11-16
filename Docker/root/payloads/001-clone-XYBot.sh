a1f2cfbdaaa7d3491692e1ea0e679df411072798
wuhao1477 <wuhao1477@qq.com>
2024-11-17 03:26:36 +0800

    fix: 尝试修复bug

--- Docker/root/payloads/001-clone-XYBot.sh
+++ Docker/root/payloads/001-clone-XYBot.sh
@@ -3,5 +3,4 @@
 cd /home/app || exit
 git clone https://github.com/wuhao1477/xybot.git XYBot
 cd XYBot || exit
-# 删除以下行或修改为实际存在的分支
 # git checkout dev
--- Docker/root/payloads/002-install-XYBot-dep.sh
+++ Docker/root/payloads/002-install-XYBot-dep.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-# ...existing code...
 cd /home/app/XYBot || exit
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
