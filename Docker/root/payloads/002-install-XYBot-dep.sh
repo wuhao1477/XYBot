0db153a37149199b31c14fa62ad4bc8b00c0375e
wuhao1477 <wuhao1477@qq.com>
2024-11-18 15:25:47 +0800

    fix: 更换pip源

--- Docker/root/payloads/002-install-XYBot-dep.sh
+++ Docker/root/payloads/002-install-XYBot-dep.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/app/XYBot || exit
-pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
